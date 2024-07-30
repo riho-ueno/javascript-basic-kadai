e9161e77452306a737f200762afce76a19949707
riho-ueno <fnk1025@au.com>
2024-07-30 17:37:07 +0900

    四則演算をしよう

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,7 @@
 let bodyWeight = 68;
 let height = 1.7;
+let bmi = bodyWeight/(height*height);
+
 console.log(bodyWeight)
 console.log(height)
-console.log(bodyWeight/(height*height))
+console.log(bmi)
